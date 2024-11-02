f3e2f98c712a6ce1443ff447b738f8e2e72fb6f5
sd22404 <sd22404@bristol.ac.uk>
2024-11-02 15:03:51 +0000

    shit works

--- SpookySealTeam7/Assets/Scripts/Player/PlayerMovement.cs
+++ SpookySealTeam7/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController _controller;
-    private Transform _transform;
     private Vector3 _playerVelocity;
+    private Vector3 _rotation;
     private bool _groundedPlayer;
     [SerializeField] private float playerSpeed = 5.0f;
     [SerializeField] private float mouseSpeed = 100.0f;
-    [SerializeField] private float jumpHeight = 0.5f;
+    [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         _controller = gameObject.AddComponent<CharacterController>();
-        _transform = gameObject.GetComponent<Transform>();
+        _rotation = transform.localEulerAngles;
     }
 
     // Update is called once per frame
@@ -29,18 +28,19 @@ public class PlayerMovement : MonoBehaviour
             _playerVelocity.y = 0f;
         }
         
-        Vector3 move = _transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 move = transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _controller.Move(move * (Time.deltaTime * playerSpeed));
 
-        // Makes the player jump
+        // player jump
         if (Input.GetButtonDown("Jump") && _groundedPlayer)
         {
             _playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
-
+        
+        // player rotation
         float h = Input.GetAxis("Mouse X") * (Time.deltaTime * mouseSpeed);
-        float v = Input.GetAxis("Mouse Y") * (Time.deltaTime * -mouseSpeed);
-        _transform.rotation *= Quaternion.Inverse(_transform.rotation) * Quaternion.Euler(Vector3.up * h + Vector3.right * v);
+        _rotation.y += + h;
+        transform.localEulerAngles = _rotation;
 
         _playerVelocity.y += gravityValue * Time.deltaTime;
         _controller.Move(_playerVelocity * Time.deltaTime);
