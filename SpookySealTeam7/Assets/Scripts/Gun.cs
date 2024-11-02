7876eaab10402ddb4a5fecf0da60a934586fc33c
sd22404 <sd22404@bristol.ac.uk>
2024-11-02 15:55:21 +0000

    app new house to game scene

--- SpookySealTeam7/Assets/Scripts/Gun.cs
+++ SpookySealTeam7/Assets/Scripts/Gun.cs
@@ -23,7 +23,7 @@ public class Gun : MonoBehaviour
             // Raycast from the gun to see if it hits anything
             Vector3 fwd = transform.TransformDirection(Vector3.forward);
             int maxDist = 100;
-            int layerMask = 1 << 6; // Only hit objects on the "Ghost" layer (6)
+            int layerMask = 1 << 6 + 1 << 7; // Only hit objects on the "Ghost" layer (6)
             if (Physics.Raycast(transform.position, fwd, maxDist, layerMask))
             {
                 print("Gun has hit object!");
