64d5b4cf33720db490a2e71ec246a9d7371321c8
Harry <lm22433@bristol.ac.uk>
2024-11-02 14:05:38 +0000

    New ghost wow

--- SpookySealTeam7/Assets/Scripts/Ghost/GhostAI.cs
+++ SpookySealTeam7/Assets/Scripts/Ghost/GhostAI.cs
@@ -1,64 +1,103 @@
 using System.Collections;
 using UnityEngine;
 
-public class Ghost : MonoBehaviour
+namespace Ghost
 {
-    private Renderer _ghostRenderer;
-    private Vector3 _lastPosition;
-    private bool _isFading;
-    private float _standStillTimer;
+    public class GhostAI : MonoBehaviour
+    {
+        private Vector3 _lastPosition;
+        private bool _isFading;
+        private bool _isVisible = true;
+        private bool _isPanicking;
+        private float _standStillTimer;
 
-    public int newLayer = 7; 
-    public float fadeDelay = 1f;
+        public float panicSpeed = 8f;
+        public float panicDuration = 1f;
+        public int gameLayer;
+        public int blackLightLayer = 7; 
+        public float fadeDelay = 1f;
     
-    void Start()
-    {
-        _ghostRenderer = GetComponent<Renderer>();
-        _lastPosition = transform.position;
-    }
-
-    void Update()
-    {
-        if (Vector3.Distance(transform.position, _lastPosition) > 0.01f)
+        void Start()
         {
-            _standStillTimer = 0f;
-            _isFading = false;
-            _ghostRenderer.enabled = true;
+            _lastPosition = transform.position;
+            StartCoroutine(Panic());
         }
-        else
-        {
-            _standStillTimer += Time.deltaTime;
 
-            if (_standStillTimer >= fadeDelay && !_isFading)
+        void Update()
+        {
+            if (_isPanicking)
             {
-                StandStill();
+                Move();
+            }
+            
+            if (Vector3.Distance(transform.position, _lastPosition) > 0.001f)
+            {
+                _standStillTimer = 0f;
+                _isFading = false;
+                _isVisible = true;
+                gameObject.layer = gameLayer;
+            }
+            else
+            {
+                _standStillTimer += Time.deltaTime;
+
+                if (_standStillTimer >= fadeDelay && !_isFading && !_isPanicking)
+                {
+                    StandStill();
+                }
+            }
+
+            _lastPosition = transform.position;
+        }
+
+        void StandStill()
+        {
+            if (_isVisible && !_isFading)
+            {
+                StartCoroutine(FadeOutAndChangeLayer());
             }
         }
 
-        _lastPosition = transform.position;
-    }
-
-    void StandStill()
-    {
-        if (_ghostRenderer.enabled && !_isFading)
+        void Move()
         {
-            StartCoroutine(FadeOutAndChangeLayer());
+            //TODO: Implement the movement logic for the ghost when it is panicking
+            Vector3 randomDirection = Random.insideUnitSphere; 
+            randomDirection.y = 0; 
+            randomDirection.Normalize();
+
+            transform.position += randomDirection * (panicSpeed * Time.deltaTime);
         }
-    }
+
+        IEnumerator Panic()
+        {
+            yield return new WaitForSeconds(1f);
+            _isPanicking = true;
+            yield return new WaitForSeconds(panicDuration);
+            _isPanicking = false;
+        }
+
+        public void ShineBlackLight()
+        {
+            _isFading = false;
+            _standStillTimer = 0f;
+            _isVisible = true;
+
+            StartCoroutine(Panic());
+        }
     
-    private IEnumerator FadeOutAndChangeLayer()
-    {
-        _isFading = true;
-        float elapsedTime = 0f;
-
-        while (elapsedTime < fadeDelay && _isFading)
+        private IEnumerator FadeOutAndChangeLayer()
         {
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
+            _isFading = true;
+            float elapsedTime = 0f;
 
-        gameObject.layer = newLayer;
-        _ghostRenderer.enabled = false;
-        // TODO: remove this renderer thing
+            while (elapsedTime < fadeDelay && _isFading)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            gameObject.layer = blackLightLayer;
+            _isVisible = false;
+        }
     }
 }
