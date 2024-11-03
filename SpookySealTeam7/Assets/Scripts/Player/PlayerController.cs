1495d8ade26f28faeff1d2858f4b1bebb8c07737
sd22404 <sd22404@bristol.ac.uk>
2024-11-03 00:24:20 +0000

    Various PlayerController improvements

--- SpookySealTeam7/Assets/Scripts/Player/PlayerController.cs
+++ SpookySealTeam7/Assets/Scripts/Player/PlayerController.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController _controller;
+    private GameObject _cam;
     private Gun _gun;
     private BlackLight _light;
     private Vector3 _playerVelocity;
     private Vector3 _rotation;
-    private bool _groundedPlayer;
+    private Vector3 _camRotation;
+    private bool _grounded;
+    private bool _paused;
     [SerializeField] private float playerSpeed = 5.0f;
     [SerializeField] private float mouseSpeed = 100.0f;
     [SerializeField] private float jumpHeight = 1.0f;
@@ -18,16 +21,29 @@ public class PlayerController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         _controller = gameObject.AddComponent<CharacterController>();
+        _cam = GameObject.FindGameObjectWithTag("MainCamera");
         _gun = gameObject.GetComponentInChildren<Gun>();
         _light = gameObject.GetComponentInChildren<BlackLight>();
         _rotation = transform.localEulerAngles;
+        _camRotation = _cam.transform.localEulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _groundedPlayer = _controller.isGrounded;
-        if (_groundedPlayer && _playerVelocity.y < 0)
+        // Check for pause menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Invert cursor lock status and pause
+            Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
+            _paused = !_paused;
+        }
+        
+        // Ignore movement if paused
+        if (_paused) return;
+        
+        _grounded = _controller.isGrounded;
+        if (_grounded && _playerVelocity.y < 0)
         {
             _playerVelocity.y = 0f;
         }
@@ -36,33 +52,33 @@ public class PlayerController : MonoBehaviour
         _controller.Move(move * (Time.deltaTime * playerSpeed));
 
         // player jump
-        if (Input.GetButtonDown("Jump") && _groundedPlayer)
+        if (Input.GetButtonDown("Jump") && _grounded)
         {
             _playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
         
         // player rotation
         float h = Input.GetAxis("Mouse X") * (Time.deltaTime * mouseSpeed);
-        _rotation.y += + h;
+        _rotation.y += h;
         transform.localEulerAngles = _rotation;
 
         _playerVelocity.y += gravityValue * Time.deltaTime;
         _controller.Move(_playerVelocity * Time.deltaTime);
         
-        // head rotation
+        // camera rotation
         float v = Input.GetAxis("Mouse Y") * (Time.deltaTime * mouseSpeed);
-        _rotation.x -= v;
-        _rotation.x = Mathf.Clamp(_rotation.x, -90.0f, 90.0f);
-        transform.localEulerAngles = _rotation;
+        _camRotation.x -= v;
+        _camRotation.x = Mathf.Clamp(_camRotation.x, -90.0f, 90.0f);
+        _cam.transform.localEulerAngles = _camRotation;
         
         // OTHER INPUTS
-        if (Input.GetAxis("Fire1").Equals(1.0f)) {
+        if (!Input.GetAxis("Fire1").Equals(0.0f)) {
             _gun.SetGunActive(true);
         } else {
             _gun.SetGunActive(false);
         }
 
-        if (Input.GetAxis("Fire2").Equals(1.0f)) {
+        if (!Input.GetAxis("Fire2").Equals(0.0f)) {
             _light.SetLightActive(true);
         } else {
             _light.SetLightActive(false);
